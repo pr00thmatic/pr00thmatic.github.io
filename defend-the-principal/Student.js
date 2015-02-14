a44f2ee92a587a3483fac21b7821464dda731691
vengadora <vengadoravg@gmail.com>
2015-02-14 13:05:55 -0400

    added a cover image to defend the principal

---
+++ defend-the-principal/Student.js
@@ -0,0 +1,145 @@
+var Student = function (level) {
+    var i,
+    max,
+    FEMALE = 1,
+    MALE = 0,
+    quantityBodyParts;
+
+    this.goUpstairs = false;
+    this.level = level;
+
+    this.bodyPart = [];
+    // 1: female, 0: male
+    this.gender = Math.round(Math.random());
+    quantityBodyParts = 4 + this.gender;
+
+    for (i=0; i<quantityBodyParts; i++) {
+        this.bodyPart[i] = game.add.sprite(0,0, 'student');
+        this.bodyPart[i].anchor.set(.5, 0);
+    }
+
+    // setting up animations for each bodyPart
+    this.setHead();
+    this.setHair();
+    this.setBody();
+    if (this.gender == FEMALE) {
+        this.setBoobs();
+    }
+    this.setLegs();
+
+    // the main sprite of a student are his legs.
+    // All the other sprites are legs children
+    this.sprite = this.legs;
+    this.sprite.anchor.set(.5,0);
+    this.sprite.kill();
+};
+
+Student.prototype.setHead = function () {
+    this.head = this.bodyPart[0];
+    this.head.animations.add('stand', [0]);
+};
+
+Student.prototype.setHair = function () {
+    this.hair = this.bodyPart[1];
+    this.hair.animations.add('stand', [1+this.gender]);
+}
+
+Student.prototype.setLegs = function () {
+    var i;
+
+    this.legs = this.bodyPart[2];
+    this.legs.animations.add('stand', [3]);
+    this.legs.animations.add('walk', [3,4], 3, true);
+    game.physics.arcade.enable(this.legs);
+
+    for (i=0, max=this.bodyPart.length; i<max; i++) {
+        this.bodyPart[i].animations.play('stand');
+
+        if (this.bodyPart[i] != this.legs) {
+            this.legs.addChild(this.bodyPart[i]);
+        }
+
+        this.bodyPart[i].bringToTop();
+    }
+
+}
+
+Student.prototype.setBody = function () {
+    this.body = this.bodyPart[3];
+    this.body.animations.add('stand', [6]);
+    this.body.animations.add('walk', [5,6,7,6], 3, true);
+};
+
+Student.prototype.setBoobs = function () {
+    this.boobs = this.bodyPart[4];
+    this.boobs.animations.add('stand', [8]);
+};
+
+
+
+
+Student.prototype.spawn = function (posX, posY) {
+    this.sprite.reset(posX, posY);
+    this.sprite.body.acceleration.y = 481;
+    this.sprite.body.velocity.x = 50;
+};
+
+Student.prototype.walk = function () {
+
+    if (this.legs.animations.currentAnim.name != 'walk') {
+        this.legs.animations.play('walk');
+        this.body.animations.play('walk');
+    }
+
+    if (this.legs.animations.currentAnim.frame == 3) {
+        this.moveHeadTo(0,1);
+    } else {
+        this.moveHeadTo(0,0);
+    }
+
+};
+
+Student.prototype.moveHeadTo = function (posX, posY) {
+    this.hair.x = this.head.x = posX;
+    this.hair.y = this.head.y = posY;
+};
+
+Student.prototype.stand = function () {
+    var i;
+
+    for (i=this.bodyPart.length-1; i>=0; i--) {
+        this.bodyPart[i].animations.play('stand');
+    }
+};
+
+Student.prototype.getViewDirection = function () {
+    var scaleX = this.sprite.scale.x;
+    return scaleX / Math.abs(scaleX);
+};
+
+
+Student.prototype.update = function () {
+    var building;
+
+    this.sprite.scale.set(this.level.globalScale * this.getViewDirection(),
+                          this.level.globalScale);
+
+    building = this.level.building;
+    game.physics.arcade.collide(this.sprite, building.floor);
+    game.physics.arcade.collide(this.sprite, building.walls,
+                                this.turnBack, null, this);
+
+    // fix this!
+    if (this.sprite.body.velocity.x === 0) {
+        this.stand();
+    } else {
+        this.walk();
+    }
+};
+
+Student.prototype.turnBack = function (student, wall) {
+    var viewDirection = this.getViewDirection();
+    viewDirection -= 2*viewDirection;
+    this.sprite.scale.set(viewDirection * this.level.globalScale,
+                          this.level.globalScale);
+};
---
+++ defend-the-principal/game.js
@@ -0,0 +1,39 @@
+var x = false;
+var mainState = ( function () {
+
+    var preload = function () {
+        game.load.spritesheet('student', 'assets/student.png', 12, 26);
+        game.load.tilemap('building', 'assets/map.json',
+                          null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('informatica', 'assets/informatica.png');
+
+        game.stage.smoothed = false;
+    }
+
+    var create = function () {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        this.student = new Student(this);
+        this.building = new Building();
+
+        this.student.sprite.bringToTop();
+        this.student.spawn(40*16, 34*16);
+    }
+
+    var update = function () {
+        this.student.update();
+        // game.physics.arcade.collide(this.student.sprite, this.building.floor);
+    };
+
+    return {
+        preload : preload,
+        create : create,
+        update : update,
+        globalScale : 2
+    };
+
+})();
+
+var game = new Phaser.Game(800, 640, Phaser.AUTO, 'game');
+game.state.add('main', mainState);
+game.state.start('main');
