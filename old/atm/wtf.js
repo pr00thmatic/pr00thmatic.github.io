cce3b73ef283c96d8e27fae105d4b693e1d5375f
pr00thmatic <vengadoravg@gmail.com>
2020-12-16 12:51:43 -0400

    atm is able to load screens from images

--- old/atm/wtf.js
+++ old/atm/wtf.js
@@ -1,3 +1,4 @@
+var asdf = 5;
 var game = (() => {
   var numpadNames = [
     "zero", "one", "two", "three", "four", "five",
@@ -15,36 +16,61 @@ var game = (() => {
     ));
   }
 
+  var createNumbersPanel = function (scene) {
+    numbersPanel = scene.getMeshByName("numbers screen");
+    var tex =
+        BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(numbersPanel);
+    var button = BABYLON.GUI.Button.CreateSimpleButton("but1", "");
+    scene.myStuff.button = button;
+    button.width = 1;
+    button.height = 1;
+    button.color = "white";
+    button.fontSize = 100;
+    button.background = "green";
+    tex.addControl(button);
+  }
+
+  var setupLighting = function (scene) {
+    var light = scene.getLightByName("ambience light");
+    light.intensity = 5;
+  }
+
+  var setupScreenMaterial = function (scene) {
+    screenMaterial = new BABYLON.PBRMaterial("ScreenMaterial", scene);
+    screen = scene.getMeshByName("screen");
+    screen.material = screenMaterial;
+    screen.material.unlit = true;
+  }
+
+  var goToScreen = function (screenName) {
+    screen.material.albedoTexture = new BABYLON.Texture("screens/" + screenName + ".png");
+  }
+
+  var screenMaterial;
+  var screen;
+  var numbersPanel;
+
   return {
     onSceneLoad: function (scene) {
-      // var mesh = scene.getMeshByName("one");
-      // mesh.actionManager = new BABYLON.ActionManager(scene);
-      // mesh.actionManager.registerAction(new BABYLON.ExecuteCodeAction(
-      //   BABYLON.ActionManager.OnPickTrigger,
-      //   function () {
-      //     scene.myStuff.button.textBlock.text += "1";
-      //   }
-      // ));
-
-      // registerNumpadFunction("two", 2, scene);
-
       for (var i=0; i<numpadNames.length; i++) {
         registerNumpadFunction(numpadNames[i], i, scene);
       }
-
-
-
-      var numbersPanel = scene.getMeshByName("numbers screen");
-      var tex =
-          BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(numbersPanel);
-      var button = BABYLON.GUI.Button.CreateSimpleButton("but1", "");
-      scene.myStuff.button = button;
-      button.width = 1;
-      button.height = 1;
-      button.color = "white";
-      button.fontSize = 100;
-      button.background = "green";
-      tex.addControl(button);
-    }
+      createNumbersPanel(scene);
+      setupLighting(scene);
+      setupScreenMaterial(scene);
+      goToScreen("0");
+      numbersPanel.setEnabled(false);
+      game.scene = scene;
+    },
+    screenMaterial: screenMaterial,
+    screen: screen
   }
 })();
+
+// setTimeout(() => {
+//   var material = new BABYLON.PBRMaterial("thisHasToWork", game.scene);
+//   material.unlit = true;
+//   material.albedoTexture = new BABYLON.Texture("screens/2.png");
+//   var screen = game.scene.getMeshByName("screen");
+//   screen.material = material;
+// }, 1000);
