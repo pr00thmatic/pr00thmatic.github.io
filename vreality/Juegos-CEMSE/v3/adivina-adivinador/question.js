685314a7b834c2aee92242f864ad645757e69148
pr00thmatic <vengadoravg@gmail.com>
2022-04-08 21:34:01 -0400

    sit tight

--- vreality/Juegos-CEMSE/v3/adivina-adivinador/question.js
+++ vreality/Juegos-CEMSE/v3/adivina-adivinador/question.js
@@ -9,7 +9,7 @@ var Question = (() => {
     if (!instance.holder) {
       instance.holder = scene.add.tileSprite(mainState.width/2, 140, config.width, config.height, 'line sheet').
         setOrigin(0.5, 0);
-      instance.holder.rotation = utils.randomIntBetween(-1,2) * utils.deg2Rad;
+      // instance.holder.rotation = utils.randomIntBetween(-1,2) * utils.deg2Rad;
       instance.label = Label.gimmieLabel(instance.holder, '', {
         color: '#000000',
         font: '15px Helvetica',
