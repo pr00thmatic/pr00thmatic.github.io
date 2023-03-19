824b227df91b225ec2a0870d9ad70cdc121aa309
pr00thmatic <vengadoravg@gmail.com>
2023-03-18 21:35:39 -0400

    game entries could be made

--- portfolioiguess/game-entries.js
+++ portfolioiguess/game-entries.js
@@ -1,9 +1,30 @@
 $(document).ready(() => {
+  Handlebars.registerHelper('isodd', function (value) {
+    return value % 2 == 0;
+  });
+
   var data = {
-    title: "zicko el niño feral",
-    team: "zolo io",
-    platform: "pc master race",
-    description: "zicko escapa de casa y decide no hablar con nadie nunca más, pero en su viaje descubrirá que los animales hablan más de lo que él esperaba"
+    game: [ {
+      title: "Aventuras con la Patrulla Canina",
+      team: "VReality - Arcor - Nickelodeon",
+      platform: "Web",
+      description: "9 minigames for the pups from Paw Patrol, each of them with its own infinite runner segment."
+    }, {
+      title: "Jukumari",
+      team: "Untamed Fox - VReality",
+      platform: "Android",
+      description: "Worked in porting the javascript version of Jukumari to Unity for its posterior release to the playstore"
+    }, {
+      title: "La tierra del Ekeko",
+      team: "VReality",
+      platform: "Android",
+      description: "Development of 6 minigames that simulate the fair games you can find at the Bolivian Alasitas traditional anual fair."
+    }, {
+      title: "Suri al Futuro",
+      team: "Ancestral Gods",
+      platform: "Android",
+      description: "Main developer, game designer and artist of a top-to-down adventure game featuring time travel."
+    } ]
   };
   var gameEntry = $("#game-entry-template").html();
   var compiledCode = Handlebars.compile(gameEntry);
