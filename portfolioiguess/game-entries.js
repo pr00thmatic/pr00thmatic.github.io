4c0cc615447207595cd465bb599c532844214ddf
pr00thmatic <vengadoravg@gmail.com>
2023-03-29 18:31:59 -0400

    add images and cool shadows

--- portfolioiguess/game-entries.js
+++ portfolioiguess/game-entries.js
@@ -10,6 +10,7 @@ $(document).ready(() => {
       platform: "Web",
       devSpan: "1 year of development",
       year: "2022",
+      img: "img/paw-patrol/zuma.png",
       description: "As the main developer on this project, I was responsible for implementing the core mechanics, the retopology of several models, level design, the creation of several graphic assets and sometimes even the game design  of 7 minigames targeted for children between 3 and 7 years old for each of the Pups of the Paw Patrol."
     }, {
       title: "Jukumari",
@@ -17,6 +18,7 @@ $(document).ready(() => {
       platform: "Android",
       devSpan: "2 months of development",
       year: "2020",
+      img: "img/jukumari/snake.png",
       description: "As the lead developer on this project, I was responsible for porting the original LibGDX version of Jukumari to Unity to release it on the Play Store. This involved rearranging all the graphic assets, implementing the spine animations as well as developing all of the game mechanics from scratch. With no access to the original code, I had to rely on descriptions from the original game designer to recreate the game as faithfully as possible while also making necessary modifications."
     }, {
       title: "La tierra del Ekeko",
@@ -24,6 +26,7 @@ $(document).ready(() => {
       platform: "Android",
       devSpan: "1 month of development",
       year: "2023",
+      img: "img/ekeko/all-the-minigames.png",
       description: "As a gameplay programmer and generalist, I contributed to the development of La tierra del Ekeko, a collection of 6 2D minigames that simulate the fair games of the Bolivian Alasitas. I implemented the core mechanics and level design of the minigames, integrated 3D models with 2D sprites, and worked with the team's lead programmer to integrate the levels with the economy system. This was a challenging project completed within a short development time of 1.5 months."
     }, {
       title: "Suri al Futuro",
@@ -31,6 +34,7 @@ $(document).ready(() => {
       platform: "Android",
       devSpan: "4 months of development",
       year: "2017",
+      img: "img/suri-al-futuro/canoa.png",
       description: "As the main developer, game designer, and artist of Suri al Futuro, an RPG adventure game, I was responsible for developing and implementing the core mechanics, including the dialogue system, inventory, and the girl's canoe, as well as designing the game and levels. I also created several graphic assets, added effects to the water, and animated jumping fishes. The game features a little girl traveling back and forth in time to prevent her village lake from drying up and convincing the villagers to care for the water and think about the future. The project was completed within three months."
     }, {
       title: "CybernautsDefender",
@@ -38,6 +42,7 @@ $(document).ready(() => {
       platform: "Android",
       devSpan: "4 months of development",
       year: "2020",
+      img: "img/patrulla-net/lvl7.png",
       description: "As the gameplay programmer for this 3D Platformer-Action game I was responsible for " +
         "implementing the core mechanics for three levels, each featuring a challenging boss fight with " +
         "unique attack patterns and weaknesses, as well as the final boss that culminated in an epic showdown " +
